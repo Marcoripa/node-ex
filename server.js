65fe08f8782c9992862bc802111baad8bfcf8c73
Marco Ripa <marcoripa@hotmail.it>
2022-08-15 22:57:53 +0200

    Ex-2

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-const {writeFile} = require("node:fs");
-const {Buffer} = require("node:buffer");
+// const {writeFile} = require("node:fs");
+// const {Buffer} = require("node:buffer");
 
-const data = new Uint8Array(Buffer.from("This is just some made up text"));
+// const data = new Uint8Array(Buffer.from("This is just some made up text"));
 
-writeFile("nextTextFile.txt", data, (err) => {
-  if (err) throw err;
-  console.log("The file has been saved!");
-});
+// writeFile("nextTextFile.txt", data, (err) => {
+//   if (err) throw err;
+//   console.log("The file has been saved!");
+// });
+
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+luckyDraw("Jon")
+  .then(message => console.log(message))
+  .catch(message => console.log(message))
+  .then(
+    luckyDraw("Caroline")
+      .then(message =>  console.log(message))
+      .catch(message => console.log(message))
+  )
+  .then(
+    luckyDraw("Sabrina")
+      .then(message => console.log(message))
+      .catch(message => console.log(message))
+  );
