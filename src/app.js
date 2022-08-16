2c5fd48f9b698c193cb9bee3f0ce53355c31143e
Marco Ripa <marcoripa@hotmail.it>
2022-08-16 18:16:01 +0200

    Set get, update, delete routes

--- src/app.js
+++ src/app.js
@@ -1,25 +1,59 @@
 const express = require("express");
-const {PrismaClient} = require("@prisma/client");
+const { PrismaClient } = require("@prisma/client");
 // const {validate} = require("./validation/index")
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
+//GET
 app.get("/users", async (req, res) => {
-  const users = await prisma.user.findMany()
-  
-  res.json(users)
+  const users = await prisma.user.findMany();
+
+  res.json(users);
 });
 
+//GET :id
+app.get("/users/:id", async (req, res) => {
+  const userId = Number(req.params.id);
+  const user = await prisma.user.findUnique({ where: { id: userId } });
+
+  res.json(user);
+});
+
+//POST
 app.post("/users", async (req, res) => {
-  const newUser = req.body
+  const newUser = req.body;
 
-  await prisma.user.create({data: newUser})
+  await prisma.user.create({ data: newUser });
 
-  res.status(201).json(newUser)
-})
+  res.status(201).json(newUser);
+});
 
-module.exports = app
+//UPDATE
+app.put("/users/:id", async (req, res) => {
+  const updateUser = req.body;
+  const userId = Number(req.params.id);
+  const user = await prisma.user.update({
+    where: { id: userId },
+    data: {
+      name: req.body.name,
+      description: req.body.description,
+      age: req.body.age,
+    },
+  });
+
+  res.json(user);
+});
+
+//DELETE
+app.delete("/users/:id", async (req, res) => {
+  const userId = Number(req.params.id);
+  const user = await prisma.user.delete({ where: { id: userId } });
+
+  res.json(user);
+});
+
+module.exports = app;
