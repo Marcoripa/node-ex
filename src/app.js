69f4a852b6e1b95f4831363600fe83f25cdafbb9
Marco Ripa <marcoripa@hotmail.it>
2022-08-16 15:57:48 +0200

    Add GET route

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient()
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ name: "Marco" });
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany()
+  
+  res.json(users)
 });
 
 module.exports = app
